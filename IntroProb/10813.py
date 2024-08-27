2c3c2901ea62113944213873944fdded0efb0b54
kathy5311 <kathy531@hanyang.ac.kr>
2024-08-27 22:12:42 +0900

    Update: Another Sol. 0827

--- IntroProb/10813.py
+++ IntroProb/10813.py
@@ -3,7 +3,7 @@
 import sys
 n,m = map(int,sys.stdin.readline().split())
 a=[i for i in range(1,n+1)]
-
+'''
 copy_a=a[:]
 for j in range(m):
     b,c = map(int,sys.stdin.readline().split())
@@ -12,8 +12,16 @@ for j in range(m):
     copy_a=a[:]
     #print(copy_a) 
 for i in a:
-    print(i, end=' ')
+    print(i, end=' ')'''
 '''
 a,b,c,d,e = [i for i in a]
 print(a,b,c,d,e)
 '''
+'---------Another Solution-------'
+for j in range(m):
+    b,c = list(map(int, sys.stdin.readline().split()))
+    temp = a[b]
+    a[b] = a[c]
+    a[c] = temp
+
+print(a)
