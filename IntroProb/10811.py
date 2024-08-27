a5630b3956050a76d00baff00fc97eb2c0a3afb9
kathy5311 <kathy531@hanyang.ac.kr>
2024-08-27 22:05:02 +0900

    Assi Solved: 0827

--- IntroProb/10811.py
+++ IntroProb/10811.py
@@ -3,12 +3,14 @@ import sys
 n,m = (map(int, sys.stdin.readline().split()))
 
 a=[i for i in range(1,n+1)]
-copy_a = a[:]
 for _ in range(m):
+    
     b,c = (map(int, sys.stdin.readline().split()))
-    copy_a.reverse()
-    a[b-1:c]=copy_a[b-1:c]
-    copy_a=a[:]
+    
+    temp=a[b-1:c]
+    temp.reverse()
+    for i in range(0,c-b+1):
+        a[b-1+i] = temp[i]
 
 for i in a:
     print(i, end=' ')
