9a35e62b24a8a57b38e48d25b2c8b582c0f2e9b2
kathy5311 <kathy531@hanyang.ac.kr>
2024-09-02 16:38:04 +0900

    Update: 0901 solved

--- IntroProb/1546.py
+++ IntroProb/1546.py
@@ -1,12 +1,16 @@
 import sys
-import math
+
 n = int(sys.stdin.readline())
 score = list(map(int, sys.stdin.readline().split()))
 
 def mean_func(*score):
-    a= max(score)
-    for i in range(len(score)):
-        score[i] = score[i]/a*100
-    summing=score.sum()
-    return summing/len(score)
+    a= max(score[0])
+    #print(score)
+    score1 = score[0]
+    #print(score1)
+    summing=0
+    for i in range(len(score1)):
+        score1[i] = (score1[i])/a*100
+        summing+=score1[i]
+    return summing/len(score1)
 print(mean_func(score))   
