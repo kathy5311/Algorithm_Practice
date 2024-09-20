2dc8fe5a1f4480af6f68cac32cb1d75ad5814bc8
kathy5311 <kathy531@hanyang.ac.kr>
2024-09-21 08:23:44 +0900

    Update: 0921 Sol

--- IntroProb/2839.py
+++ IntroProb/2839.py
@@ -2,23 +2,24 @@ import sys
 
 N =int(sys.stdin.readline())
 
+num_bag=[]
+# 5kg로만 구성
 
-if (N%5==0):
-    print(N//5)
+if N%5==0:
+    num_bag.append(N//5)
+if N%3==0:
+    num_bag.append(N//3)
+
+count=0
+while 1:
+    N=N-5
+    count+=1
+    if N <0:
+        break
+    if N % 3 == 0:
+        num_bag.append(count+(N//3))
+
+if len(num_bag)==0:
+    print(-1)      
 else:
-    if N%5 >= 3:
-        raw=5*(N//5)
-        if (N-raw)%3==0:
-            print(((N-raw)//3)+(N//5))
-        else:
-            print(-1)
-    
-    elif N%5 <3:
-        raw=5*((N//5)-1)
-        if (N-raw)%3==0:
-            print(((N-raw)//3)+((N//5)-1))
-        else:
-            if ((N-(N//5))%3==0):
-                print(N//3)
-            else:
-                print(-1)
+    print(min(num_bag))
