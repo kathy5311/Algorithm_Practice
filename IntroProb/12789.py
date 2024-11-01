49cd9ce3f0aa80657a9664df0c61198de2a803f8
kathy5311 <kathy531@hanyang.ac.kr>
2024-11-01 15:31:04 +0900

    1101 Assi Sol

--- IntroProb/12789.py
+++ IntroProb/12789.py
@@ -2,3 +2,18 @@ import sys
 
 N = input()
 line = list(map(int, sys.stdin.readline().split()))
+
+stack =[]
+now_turn = min(line)
+for value in line:
+    stack.append(value)
+    while stack and stack[-1] == now_turn:
+        stack.pop()
+        now_turn+=1
+
+if stack:
+    print("Sad")
+else:
+    print("Nice")
+
+    
