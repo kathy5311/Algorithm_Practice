63e438bb8b31bb4a00ee37b76b31740861ed1109
kathy5311 <kathy531@hanyang.ac.kr>
2024-11-19 17:13:17 +0900

    1119 Not Sol

---
+++ IntroProb/2108.py
@@ -0,0 +1,29 @@
+import sys
+from collections import Counter
+N = int(sys.stdin.readline().strip())
+
+num_list=[]
+for _ in range(N):
+    a = int(sys.stdin.readline().strip())
+    num_list.append(a)
+
+num_list.sort()
+sum_num = sum(num_list)
+len_num = len(num_list)
+
+print(round((sum_num/len_num)))
+print(num_list[len(num_list)//2])
+#최빈값
+cnt = Counter(num_list)
+a,b=max(cnt.items())
+bin_list=[]
+for i,j in cnt.items():
+    if j == b:
+        bin_list.append(i)
+bin_list.sort()
+if len(bin_list)<2:
+    print(bin_list[0])
+else:
+    print(bin_list[1])
+#범위
+print(max(num_list)-min(num_list))
