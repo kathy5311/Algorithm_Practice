bf58bc7b05f7fe2aba8696c88c12c7993efb38e4
kathy5311 <kathy531@hanyang.ac.kr>
2025-02-25 21:33:15 +0900

    0225 AssiSol

---
+++ IntroProb/prog_greedy.py
@@ -0,0 +1,29 @@
+#조이스틱
+name='JBAAN'
+
+def solution(name):
+    answer=0
+    
+    for ch in name:
+        answer+=min(ord(ch)-ord('A'),ord('Z')-ord(ch)+1)
+    
+    cursor_move=len(name)-1
+    for i in range(len(name)):
+        next_i = i+1
+
+        while (next_i<len(name)) and (name[next_i]=='A'):
+            next_i+=1
+        
+        distance = min(i, len(name)-next_i)
+        cursor_move = min(cursor_move, i+len(name)-next_i+distance)
+    answer+=cursor_move
+
+    return answer
+
+
+        
+    
+
+
+    return answer
+print(solution(name))
